8e0e74c3b41737fbabb7aed4a33aac40530047c0
Nuno Alexandre Goncalves <goncalvesnuno01@gmail.com>
2026-04-08 23:55:08 +0100

    Add: min-cost-to-connect-points - submission-1

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-1.py
@@ -0,0 +1,39 @@
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        '''
+        minimum spanning tree
+        pick any point, visited, choose the minimum edge/manhattan distance
+        '''
+        dist = collections.defaultdict(list)
+        for i, (xi, yi) in enumerate(points):
+            for j, (xj, yj) in enumerate(points):
+                if i == j:
+                    continue
+                distance = abs(xi - xj) + abs(yi - yj)
+                dist[i].append((j, distance))
+        
+        total = 0
+        edge = -1
+
+        priorityQueue = [(0, 0)] # weight, index
+        visited = set()
+
+        while priorityQueue and edge < len(points) - 1:
+            w, i = heapq.heappop(priorityQueue)
+
+            if i in visited:
+                continue
+            
+            visited.add(i)
+            total += w
+            edge += 1
+
+            for neighbour, weight in dist[i]:
+                if neighbour in visited:
+                    continue
+                heapq.heappush(priorityQueue, (weight, neighbour))
+        
+        return total
+
+
+        

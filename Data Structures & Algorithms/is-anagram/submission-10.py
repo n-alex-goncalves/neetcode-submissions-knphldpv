6e2cf8601617143833dc19d32a5260133780446c
Nuno Alexandre Goncalves <goncalvesnuno01@gmail.com>
2026-05-24 21:33:35 +0100

    Add: is-anagram - submission-10

---
+++ Data Structures & Algorithms/is-anagram/submission-10.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        def getFreq(s):
+            lst = [0] * 27
+            for character in s:
+                lst[ord(character) - 96] += 1
+            return lst
+        
+        return getFreq(s) == getFreq(t)
